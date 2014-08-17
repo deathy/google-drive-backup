5990313c32459aed21d967ccb4c18e575749b389
deathy <deathy@deathy.ro>
2014-08-18 00:35:49 +0300

    simpler backup, export all the things
    Removed logging, command-line flags and other options.
    For google documents download/export with all available export options, use mime-type mapping for file extensions.

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 Google Drive Backup
 ===================
 
-A python script to sync your google drive contents.
+A python script to backup your google drive contents.
 
 ## Features
-* You can Download your entire google drive or any given folder
-* Downloads a file only if it has been modified since last download
-* Logs all actions (optional)
+* Downloads entire Google Drive folder all the time
+* Downloads to fixed `downloads/` sub folder
+* File type behavior:
+    * normal files which have been uploaded are downloaded as-is
+    * google documents (docs,spreadsheets,drawings,presentations,etc) are exported in all possible export formats ( this
+    means more files and space, but exporting all gives you a choice later on)
 * Uses OAuth2 authentication and can remember authentication
-* Exports and downloads native Google Documents, Spreadsheets, and Presentations, as Microsoft Office, Open Office or PDF documents
+* _'feature'_: doesn't check modification times, always downloads everything. There are other better diff/backup tools 
+for incremental backup which you can use after 
 
 ## Requirements
 * Google API Python library. To install run
@@ -20,16 +24,3 @@ A python script to sync your google drive contents.
 * Save it as `client_secrets.json`.
 * Now, if you run `python drive.py`, a browser window/tab will open for you to authenticate the script.
 * Once authentication is done, the script will start downloading your *My Drive*. Refer the next section for more options.
-
-## Options
-Following command line options are available.
-
-**--destination** - Path to the folder where the files have to be downloaded to. If not specified, a folder named `downloaded` is created in the current directory.
-
-**--debug** - If present (accepts no value), every step will be logged to the log file.
-
-**--logfile** - Path to the file to which the logs should be written to. By default, writes to `drive.log` in the current directory. The file will be overwritten every time the script is run.
-
-**--drive_id** ID of the folder which you want to download. By default, entire "My Drive" is downloaded.
-
-**--export** Defines the export format for native Google Documents, Spreadsheets, and Presentations: Microsoft Office (MSO), OpenOffice (OO), or PDF.  By default, export to OpenOffice format (.odt for Gdocs; .ods for Gspreadsheets; .pptx for presentations).
--- drive.py
+++ drive.py
@@ -1,38 +1,27 @@
-"""Simple command-line sample for the Google Drive API.
+"""Download your entire Google Drive folder
 
-Command-line application that retrieves the list of files in google drive.
+Command-line application that downloads entire Google Drive folder to a 'downloaded/' folder.
+All possible exports are performed.
 
 Usage:
     $ python drive.py
-
-You can also get help on all the command-line flags the program understands
-by running:
-
-    $ python drive.py --help
-
-To get detailed log output run:
-
-    $ python drive.py --logging_level=DEBUG
 """
 
-__author__ = 'viky.nandha@gmail.com (Vignesh Nandha Kumar); jeanbaptiste.bertrand@gmail.com (Jean-Baptiste Bertrand)'
+__author__ = 'viky.nandha@gmail.com (Vignesh Nandha Kumar); jeanbaptiste.bertrand@gmail.com (Jean-Baptiste Bertrand); Cristian Vat'
 
-import gflags, httplib2, logging, os, pprint, sys, re, time
+import httplib2, os, sys, re, time
 
 from apiclient.discovery import build
 from oauth2client.file import Storage
 from oauth2client.client import AccessTokenRefreshError, flow_from_clientsecrets
 from oauth2client.tools import run
 
-
-FLAGS = gflags.FLAGS
-
 # CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
 # application, including client_id and client_secret, which are found
 # on the API Access tab on the Google APIs
 # Console <http://code.google.com/apis/console>
 
-CLIENT_SECRETS = open('path_secrets.conf').read() #the path to the client_secrets has to be defined in this conf file
+CLIENT_SECRETS = "client_secrets.json"  # the path to the client_secrets has to be defined in this conf file
 
 # Helpful message to display in the browser if the CLIENT_SECRETS file
 # is missing.
@@ -50,101 +39,71 @@ with information from the APIs Console <https://code.google.com/apis/console>.
 
 # Set up a Flow object to be used if we need to authenticate.
 FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
-    scope='https://www.googleapis.com/auth/drive',
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+                               scope='https://www.googleapis.com/auth/drive',
+                               message=MISSING_CLIENT_SECRETS_MESSAGE)
 
+extensions = {
+    # generic mime types and extensions
+    'text/html': '.html',
+    'text/plain': '.txt',
+    'image/jpeg': '.jpg',
+    'image/svg+xml': '.svg',
+    'image/png': '.png',
+    'application/pdf': '.pdf',
+    'application/rtf': '.rtf',
+    # microsoft office document mime types and extensions ( http://technet.microsoft.com/en-us/library/cc179224.aspx )
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document':'.docx',
+    'application/vnd.ms-word.document.macroEnabled.12':'.docm',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.template':'.dotx',
+    'application/vnd.ms-word.template.macroEnabled.12':'.dotm',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':'.xlsx',
+    'application/vnd.ms-excel.sheet.macroEnabled.12':'.xlsm',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.template':'.xltx',
+    'application/vnd.ms-excel.template.macroEnabled.12':'.xltm',
+    'application/vnd.ms-excel.sheet.binary.macroEnabled.12':'.xlsb',
+    'application/vnd.ms-excel.addin.macroEnabled.12':'.xlam',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation':'.pptx',
+    'application/vnd.ms-powerpoint.presentation.macroEnabled.12':'.pptm',
+    'application/vnd.openxmlformats-officedocument.presentationml.slideshow':'.ppsx',
+    'application/vnd.ms-powerpoint.slideshow.macroEnabled.12':'.ppsm',
+    'application/vnd.openxmlformats-officedocument.presentationml.template':'.potx',
+    'application/vnd.ms-powerpoint.template.macroEnabled.12':'.potm',
+    'application/vnd.ms-powerpoint.addin.macroEnabled.12':'.ppam',
+    'application/vnd.openxmlformats-officedocument.presentationml.slide':'.sldx',
+    'application/vnd.ms-powerpoint.slide.macroEnabled.12':'.sldm',
+    'application/onenote':'.onetoc',
+    # open document (open office) mime types and extensions ( http://en.wikipedia.org/wiki/OpenDocument_technical_specification#Documents )
+    'application/vnd.oasis.opendocument.text': '.odt',
+    'application/vnd.oasis.opendocument.spreadsheet': '.ods',
+    'application/x-vnd.oasis.opendocument.spreadsheet': '.ods',
+    'application/vnd.oasis.opendocument.presentation': '.odp',
+    'application/vnd.oasis.opendocument.graphics': '.odg',
+    'application/vnd.oasis.opendocument.chart': '.odc',
+    'application/vnd.oasis.opendocument.formula': 'odf',
+    'application/vnd.oasis.opendocument.image': '.odi',
+    'application/vnd.oasis.opendocument.text-master': '.odm',
+    'application/vnd.sun.xml.base': '.odm',
+    'application/vnd.oasis.opendocument.base': '.odm',
+    'application/vnd.oasis.opendocument.database': 'odb'
+}
 
-# The gflags module makes defining command-line options easy for
-# applications. Run this program with the '--help' argument to see
-# all the flags that it understands.
-gflags.DEFINE_enum('logging_level', 'ERROR',
-                   ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                   'Set the level of logging detail.')
-gflags.DEFINE_string('destination', 'downloaded/', 'Destination folder location', short_name='d')
-gflags.DEFINE_boolean('debug', False, 'Log folder contents as being fetched')
-gflags.DEFINE_string('logfile', 'drive.log', 'Location of file to write the log')
-gflags.DEFINE_string('drive_id', 'root', 'ID of the folder whose contents are to be fetched')
-gflags.DEFINE_enum('export', 'OO', ['PDF', 'OO', 'MSO'], 'Export format. Export to PDF, OpenOffice, or MS Office format')
-
-
-
-def export_type():
-
-#Defining a "export_format" dictionary:
-# *key = source mimeType of the Gdoc
-# *value = a list of the target mimeType (index 0) + the target file extension (index 1)
-#Values change according to the "export format" defined by the user.
-#Maybe is there a cleaner way to do this?
-    if FLAGS['export'].value == 'MSO':
-        return {
-        'application/vnd.google-apps.document': ('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'docx'),
-        'application/vnd.google-apps.drawing': ('image/png', 'png'),
-    #'application/vnd.google-apps.form': ('text/csv', 'csv'), : Can it be exported?
-    #'application/vnd.google-apps.fusiontable' : Can it be exported?
-        'application/vnd.google-apps.photo': ('image/png', 'png'),
-        'application/vnd.google-apps.presentation': ('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'pptx'),
-        'application/vnd.google-apps.spreadsheet': ('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'xlsx')
-        
-        }
-        
-    elif FLAGS['export'].value == 'OO':
-        return {
-        'application/vnd.google-apps.document': ('application/vnd.oasis.opendocument.text', 'odt'),
-        'application/vnd.google-apps.drawing': ('image/png', 'png'),
-    #'application/vnd.google-apps.form': ('text/csv', 'pdf'),
-    #'application/vnd.google-apps.fusiontable'
-        'application/vnd.google-apps.photo': ('image/png', 'png'),
-        'application/vnd.google-apps.presentation': ('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'pptx'),
-        'application/vnd.google-apps.spreadsheet': ('application/x-vnd.oasis.opendocument.spreadsheet', 'ods')
-        
-        }
-        
-    elif FLAGS['export'].value == 'PDF':
-        return {
-        'application/vnd.google-apps.document': ('application/pdf', 'pdf'),
-        'application/vnd.google-apps.drawing': ('image/png', 'png'),
-    #'application/vnd.google-apps.form': ('text/csv', 'csv'),
-    #'application/vnd.google-apps.fusiontable' :
-        'application/vnd.google-apps.photo': ('image/png', 'png'),
-        'application/vnd.google-apps.presentation': ('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'pdf'),
-        'application/vnd.google-apps.spreadsheet': ('application/pdf', 'pdf')
-        
-        }
-    
-
-def open_logfile():
-    if not re.match('^/', FLAGS.logfile):
-        FLAGS.logfile = FLAGS.destination + FLAGS.logfile
-    global LOG_FILE
-    LOG_FILE = open(FLAGS.logfile, 'w+')
-
-def log(str):
-    LOG_FILE.write((str + '\n').encode('utf8'))
 
 def ensure_dir(directory):
     if not os.path.exists(directory):
-        log("Creating directory: %s" % directory)
+        print("Creating directory: %s" % directory)
         os.makedirs(directory)
+
 
 def is_google_doc(drive_file):
     return True if re.match('^application/vnd\.google-apps\..+', drive_file['mimeType']) else False
 
-def is_file_modified(drive_file, local_file):
-    if os.path.exists(local_file):
-        rtime = time.mktime(time.strptime(drive_file['modifiedDate'], '%Y-%m-%dT%H:%M:%S.%fZ'))
-        ltime = os.path.getmtime(local_file)
-        return rtime > ltime
-    else:
-        return True
 
 def get_folder_contents(service, http, folder, base_path='./', depth=0):
-    
-    if FLAGS.debug:
-        log("\n" + ' ' * depth + "Getting contents of folder %s" % folder['title'])
+    print("\n" + ' ' * depth + "Getting contents of folder %s" % folder['title'])
     try:
         folder_contents = service.files().list(q="'%s' in parents" % folder['id']).execute()
     except:
-        log("ERROR: Couldn't get contents of folder %s. Retrying..." % folder['title'])
+        print("ERROR: Couldn't get contents of folder %s. Retrying..." % folder['title'])
         get_folder_contents(service, http, folder, base_path, depth)
         return
     folder_contents = folder_contents['items']
@@ -156,122 +115,94 @@ def get_folder_contents(service, http, folder, base_path='./', depth=0):
     def is_folder(item):
         return item['mimeType'] == 'application/vnd.google-apps.folder'
 
-    if FLAGS.debug:
-        for item in folder_contents:
-            if is_folder(item):
-                log(' ' * depth + "[] " + item['title'])
-            else:
-                log(' ' * depth + "-- " + item['title'])
-        
-    ensure_dir(dest_path)
-    export_mimeType = export_type()
-    for item in filter(is_file, folder_contents):
-        #Check if it is a native Gdoc
-        if is_google_doc(item):
-            #Check if it is an exportable document
-            if item['mimeType'] in export_mimeType.keys():
-                
-                extension = export_mimeType[item['mimeType']][1]
-                full_path = dest_path + item['title'].replace('/', '_') + os.extsep + extension
-            else:
-                full_path = dest_path + item['title'].replace('/', '_')
+    for item in folder_contents:
+        if is_folder(item):
+            print(' ' * depth + "[] " + item['title'])
         else:
-            full_path = dest_path + item['title'].replace('/', '_')
+            print(' ' * depth + "-- " + item['title'])
 
-
-        if is_file_modified(item, full_path):
-            is_file_new = not os.path.exists(full_path)
-            
-            if download_file(service, item, dest_path):
-                if is_file_new:
-                    log("Created %s" % full_path)
+    ensure_dir(dest_path)
+    for item in filter(is_file, folder_contents):
+        # Check if it is a native Gdoc
+        if 'exportLinks' in item:
+            for exportMimeType in item['exportLinks'].keys():
+                if not exportMimeType in extensions:
+                    print("Couldn't find mime-type mapping: " + exportMimeType)
+                    continue
+                extension = extensions[exportMimeType]
+                full_path = dest_path + clean_file_name(item['title']) + extension
+                if download_file(service, item, dest_path, exportMimeType):
+                    print("Created %s" % full_path)
                 else:
-                    log("Updated %s" % full_path)
+                    print("ERROR while saving %s" % full_path)
+        if 'downloadUrl' in item:
+            full_path = dest_path + clean_file_name(item['title'])
+            if download_file(service, item, dest_path, 'DIRECT_DOWNLOAD'):
+                print("Created %s" % full_path)
             else:
-                log("ERROR while saving %s" % full_path)
+                print("ERROR while saving %s" % full_path)
 
     for item in filter(is_folder, folder_contents):
-        get_folder_contents(service, http, item, dest_path, depth+1)
+        get_folder_contents(service, http, item, dest_path, depth + 1)
 
 
-def download_file(service, drive_file, dest_path):
+def clean_file_name(original):
+    return original.replace('/', '_').replace('?', '_')
+
+
+def download_file(service, drive_file, dest_path, dest_mimeType):
     """Download a file's content.
 
-Args:
-service: Drive API service instance.
-drive_file: Drive File instance.
+    Args:
+    service: Drive API service instance.
+    drive_file: Drive File instance.
 
-Returns:
-File's content if successful, None otherwise.
-"""
-        
-    #Showing progress
+    Returns:
+    File's content if successful, None otherwise.
+    """
+    # Showing progress
     print drive_file['title'] + " download in progress..."
-    export_mimeType = export_type()    
     if is_google_doc(drive_file):
-        
-           
-        if drive_file['mimeType'] in export_mimeType.keys():
-            extension = export_mimeType[drive_file['mimeType']][1]
+        if 'exportLinks' in drive_file:
+            if not dest_mimeType in extensions:
+                return False
+            extension = extensions[dest_mimeType]
             file_location = dest_path + drive_file['title'].replace('/', '_') + os.extsep + extension
-        #From the "export_mimeType" dictionary, retrieving data corresponding to the source mimeType:
-            source_mimeType = drive_file['mimeType']
-            dest_mimeType = export_mimeType[source_mimeType]
-            
-            #Retrieving the target mimeType (index 0) for putting it as a download url parameter
-            download_url = drive_file.get('exportLinks')[dest_mimeType[0]]
-        
+            # From the "export_mimeType" dictionary, retrieving data corresponding to the source mimeType:
+            # Retrieving the target mimeType (index 0) for putting it as a download url parameter
+            download_url = drive_file.get('exportLinks')[dest_mimeType]
         else:
-                #if source mimeType is unknown, the google doc can't be exported
-            print drive_file['title'] + " can't be exported (" + drive_file['mimeType'] + " mimeType)"        
+            # if source mimeType is unknown, the google doc can't be exported
+            print drive_file['title'] + " can't be exported (" + drive_file['mimeType'] + " mimeType)"
             return False
-                
-            
     else:
-        
         file_location = dest_path + drive_file['title'].replace('/', '_')
-    
         download_url = drive_file['downloadUrl']
-        
     if download_url:
         try:
             resp, content = service._http.request(download_url)
         except httplib2.IncompleteRead:
-            log('Error while reading file %s. Retrying...' % drive_file['title'].replace('/', '_'))
-            download_file(service, drive_file, dest_path)
+            print('Error while reading file %s. Retrying...' % drive_file['title'].replace('/', '_'))
+            download_file(service, drive_file, dest_path, dest_mimeType)
             return False
-
         if resp.status == 200:
-
             try:
                 target = open(file_location, 'wb')
             except:
-                log("Could not open file %s for writing. Please check permissions." % file_location)
+                print("Could not open file %s for writing. Please check permissions." % file_location)
                 return False
-
             target.write(content)
             target.close()
             return True
         else:
-            log('An error occurred: %s' % resp)
+            print('An error occurred: %s' % resp)
             return False
     else:
         # The file doesn't have any content stored on Drive.
         return False
 
 
-def main(argv):
-    # Let the gflags module process the command-line arguments
-    try:
-        argv = FLAGS(argv)
-        
-    except gflags.FlagsError, e:
-        print '%s\\nUsage: %s ARGS\\n%s' % (e, argv[0], FLAGS)
-        sys.exit(1)
-
-    # Set the logging according to the command-line flag
-    logging.getLogger().setLevel(getattr(logging, FLAGS.logging_level))
-
+def main():
     # If the Credentials don't exist or are invalid run through the native client
     # flow. The Storage object will ensure that if successful the good
     # Credentials will get written back to a file.
@@ -288,14 +219,15 @@ def main(argv):
 
     service = build("drive", "v2", http=http)
 
-    open_logfile()
-
     try:
-        start_folder = service.files().get(fileId=FLAGS.drive_id).execute()
-        get_folder_contents(service, http, start_folder, FLAGS.destination)
+        start_folder = service.files().get(fileId='root').execute()
+        if not os.path.exists('downloaded/'):
+            os.makedirs('downloaded/')
+        get_folder_contents(service, http, start_folder, 'downloaded/')
     except AccessTokenRefreshError:
         print ("The credentials have been revoked or expired, please re-run"
                "the application to re-authorize")
 
+
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
